045f6da9d2877a1775174ba5ae705ada4f115e80
dylanxhernandez <dylan.x.h.developer@outlook.com>
2023-08-16 00:30:22 -0400

    Add neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+local set = vim.opt
+
+set.number = true
+set.relativenumber = true
+set.autoindent = true
+set.tabstop = 4
+set.shiftwidth = 4
+set.softtabstop = 4
+set.termguicolors = true
+
+vim.cmd("colorscheme modberg")
