ff399d82ce3780e96a06fc87eaf1c54157a29220
dylanxhernandez <dylan.x.h.developer@gmail.com>
2023-09-05 16:02:55 -0400

    feat: update autoinstall to include background image

--- autoinstall.sh
+++ autoinstall.sh
@@ -30,6 +30,20 @@ for source_folder in "${!config_folders[@]}"; do
     cp -R "$full_source_path"* "$full_destination_path"
 
     echo "Copied files from $full_source_path to $full_destination_path"
+
+    # Check if i3 is being copied and copy the image file
+    if [ "$destination_folder" = "i3" ]; then
+        i3_image_file="solid.png"  # Update this with the actual image file name
+        pictures_dir="$HOME/Pictures"
+
+        # Create the ~/Pictures/ directory if it doesn't exist
+        mkdir -p "$pictures_dir"
+
+        # Copy the i3 image file to ~/Pictures/
+        cp "$full_source_path$i3_image_file" "$pictures_dir"
+
+        echo "Copied i3 image file to $pictures_dir"
+    fi
 done
 
 echo "Configuration files copied successfully."
